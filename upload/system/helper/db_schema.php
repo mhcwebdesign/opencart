44c9a81cd28bdf3dae0ee7932bb0c86781018175
Cl!cker <info@clicker.com.ua>
2026-04-21 23:00:55 +0300

    Revert "[4.x.x.x] Increase description and settings fields size"
    This reverts commit 58135fd20cc91b50763e796a9305e7f1f40fd4b2.

--- upload/system/helper/db_schema.php
+++ upload/system/helper/db_schema.php
@@ -685,7 +685,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'description',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name' => 'image',
@@ -950,7 +950,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'description',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name' => 'image',
@@ -1230,7 +1230,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'description',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name' => 'meta_title',
@@ -4118,7 +4118,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'setting',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			]
 		],
 		'primary' => [
@@ -5352,7 +5352,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'description',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name' => 'tag',
@@ -6479,7 +6479,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'data',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name' => 'expire',
@@ -6525,7 +6525,7 @@ function oc_db_schema() {
 			],
 			[
 				'name' => 'value',
-				'type' => 'mediumtext'
+				'type' => 'text'
 			],
 			[
 				'name'    => 'serialized',
